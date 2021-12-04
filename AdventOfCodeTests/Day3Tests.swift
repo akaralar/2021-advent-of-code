a179d8886522ea8fc7b00d1b2a25ba31ba590740
Ahmet Karalar <akaralar@gmail.com>
2021-12-04 02:27:07 +0100

    Add day 3 part 2 tests

--- AdventOfCodeTests/Day3Tests.swift
+++ AdventOfCodeTests/Day3Tests.swift
@@ -31,7 +31,26 @@ class Day3Tests: XCTestCase {
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        XCTAssertEqual(
+            day.part2(
+                """
+                00100
+                11110
+                10110
+                10111
+                10101
+                01111
+                00111
+                11100
+                10000
+                11001
+                00010
+                01010
+                """
+            ) as? Int,
+            230
+        )
+
     }
 
 }
