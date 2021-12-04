985e88e470cb150d6e1b898a39cf6198800aa773
Ahmet Karalar <akaralar@gmail.com>
2021-12-04 02:27:07 +0100

    Solve day 3 part 2

--- AdventOfCode/Days/Day3.swift
+++ AdventOfCode/Days/Day3.swift
@@ -33,6 +33,36 @@ final class Day3: Day {
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        let lines = input.lines
+
+        var ogrLines = lines
+        var cursor = ogrLines.first!.startIndex
+        while ogrLines.count != 1 {
+            let counts = digitCountSets(for: ogrLines)
+            let ogrBitCriteria = counts.map { $0.count(for: "1") >= $0.count(for: "0") ? "1" : "0" }.joined()
+            ogrLines = ogrLines.filter { $0[cursor] == ogrBitCriteria[cursor] }
+            cursor = ogrBitCriteria.index(after: cursor)
+        }
+
+        var co2srLines = lines
+        cursor = co2srLines.first!.startIndex
+        while co2srLines.count != 1 {
+            let counts = digitCountSets(for: co2srLines)
+            let co2srBitCriteria = counts
+                .map { $0.count(for: "0") <= $0.count(for: "1") ? "0" : "1" }
+                .joined()
+
+            co2srLines = co2srLines.filter { $0[cursor] == co2srBitCriteria[cursor] }
+            cursor = co2srBitCriteria.index(after: cursor)
+        }
+
+        guard
+            let ogr = ogrLines.first,
+            let co2sr = co2srLines.first,
+            let ogrDecimal = Int(ogr, radix: 2),
+            let co2srDecimal = Int(co2sr, radix: 2)
+        else { return 0 }
+
+        return ogrDecimal * co2srDecimal
     }
 }
