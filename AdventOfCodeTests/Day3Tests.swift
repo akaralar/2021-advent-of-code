ab904d8156f9dc3453253314023733d0a77cb134
Ahmet Karalar <akaralar@gmail.com>
2021-12-04 02:27:07 +0100

    Add day 3 part 1 tests

--- AdventOfCodeTests/Day3Tests.swift
+++ AdventOfCodeTests/Day3Tests.swift
@@ -9,7 +9,25 @@ class Day3Tests: XCTestCase {
     let day = Day3()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        XCTAssertEqual(
+            day.part1(
+                """
+                00100
+                11110
+                10110
+                10111
+                10101
+                01111
+                00111
+                11100
+                10000
+                11001
+                00010
+                01010
+                """
+            ) as? Int,
+            198
+        )
     }
 
     func testPart2() throws {
