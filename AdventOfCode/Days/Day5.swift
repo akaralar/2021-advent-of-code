e2c7df227a705119edaec6e901edd95ddcf314d5
Ahmet Karalar <akaralar@gmail.com>
2021-12-05 22:03:09 +0100

    Add solution for day 5 part 1

--- AdventOfCode/Days/Day5.swift
+++ AdventOfCode/Days/Day5.swift
@@ -4,10 +4,46 @@
 //
 
 import Foundation
+import AOCToolbox
+
+struct Point {
+    let x: Int
+    let y: Int
+}
+
+struct OceanFloor {
+    let matrix: Matrix<Int>
+}
+
+extension Point: StringInitializable {
+    public init?(_ description: String) {
+        let coordinates = description.components(separatedBy: ",")
+        x = Int(coordinates[0])!
+        y = Int(coordinates[1])!
+    }
+}
 
 final class Day5: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let vents: [(Point, Point)] = input.rows(lineDelimiter: "\n", interItemDelimiter: " -> ")
+        var floor = Matrix<Int>()
+
+        for vent in vents {
+            let (start, end) = vent
+            if start.x == end.x {
+                for y in min(start.y, end.y) ... max(start.y, end.y) {
+                    floor[start.x, y, default: 0] += 1
+                }
+            } else if start.y == end.y {
+                for x in min(start.x, end.x) ... max(start.x, end.x) {
+                    floor[x, start.y, default: 0] += 1
+                }
+            } else {
+                // skip for part 1
+            }
+        }
+
+        return floor.elements { $0 >= 2 }.count
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
