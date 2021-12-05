3b7c5d6379387204d57c04a313969bbd33fc0934
Ahmet Karalar <akaralar@gmail.com>
2021-12-05 22:33:40 +0100

    Add solution for day 5 part 2

--- AdventOfCode/Days/Day5.swift
+++ AdventOfCode/Days/Day5.swift
@@ -47,6 +47,41 @@ final class Day5: Day {
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        let vents: [(Point, Point)] = input.rows(lineDelimiter: "\n", interItemDelimiter: " -> ")
+        var floor = Matrix<Int>()
+
+        for vent in vents {
+            let (start, end) = vent
+            if start.x == end.x {
+                for y in min(start.y, end.y) ... max(start.y, end.y) {
+                    floor[start.x, y, default: 0] += 1
+                }
+            } else if start.y == end.y {
+                for x in min(start.x, end.x) ... max(start.x, end.x) {
+                    floor[x, start.y, default: 0] += 1
+                }
+            } else {
+                if (start.x > end.x && start.y > end.y) || (start.x < end.x && start.y < end.y) {
+                    let minX = min(start.x, end.x)
+                    let minY = min(start.y, end.y)
+                    for i in 0 ... abs(start.x - end.x) {
+                        let x = minX + i
+                        let y = minY + i
+                        floor[x, y, default: 0] += 1
+                    }
+                } else {
+                    let minX = min(start.x, end.x)
+                    let maxY = max(start.y, end.y)
+                    for i in 0 ... abs(start.x - end.x) {
+                        let x = minX + i
+                        let y = maxY - i
+                        floor[x, y, default: 0] += 1
+                    }
+
+                }
+            }
+        }
+
+        return floor.elements { $0 >= 2 }.count
     }
 }
