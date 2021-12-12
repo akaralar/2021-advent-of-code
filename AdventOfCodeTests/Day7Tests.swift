93e549fe39f807dac7acd07d2c01fb57a549bcab
Ahmet Karalar <akaralar@gmail.com>
2021-12-12 17:30:22 +0100

    Add tests for day 7 part 1

--- AdventOfCodeTests/Day7Tests.swift
+++ AdventOfCodeTests/Day7Tests.swift
@@ -9,7 +9,14 @@ class Day7Tests: XCTestCase {
     let day = Day7()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        XCTAssertEqual(
+            day.part1(
+                """
+                16,1,2,0,4,2,7,1,2,14
+                """
+            ) as? Int,
+            37
+        )
     }
 
     func testPart2() throws {
