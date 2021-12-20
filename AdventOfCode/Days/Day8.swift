067e54c0051a0cb1dc42192e21767b1c483cb934
Ahmet Karalar <akaralar@gmail.com>
2021-12-20 01:28:38 +0100

    Add solution for day 8 part 1

--- AdventOfCode/Days/Day8.swift
+++ AdventOfCode/Days/Day8.swift
@@ -4,10 +4,18 @@
 //
 
 import Foundation
+import AOCToolbox
 
 final class Day8: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let uniquePatternCounts: Set<Int> = [2, 3, 4, 7]
+
+        return input
+            .byNewlines
+            .map { $0.byPipes[1] }
+            .flatMap(\.bySpaces)
+            .filter { uniquePatternCounts.contains($0.count) }
+            .count
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
